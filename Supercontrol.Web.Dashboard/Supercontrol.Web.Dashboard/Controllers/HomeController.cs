05b1fccd4ae8ae4dbe46c383a52356ac522793c6
pawel-kruza <pawel.kruza@supercontrol.co.uk>
2026-07-07 16:12:55 +0200

    replace top locations

--- Supercontrol.Web.Dashboard/Supercontrol.Web.Dashboard/Controllers/HomeController.cs
+++ Supercontrol.Web.Dashboard/Supercontrol.Web.Dashboard/Controllers/HomeController.cs
@@ -1,14 +1,29 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
+using Supercontrol.Web.Dashboard.Models;
 
 namespace Supercontrol.Web.Dashboard.Controllers
 {
     public class HomeController : Controller
     {
+        private const string TopLocationsSql =
+            @"SELECT customercountry, COUNT(1) AS c
+              FROM bookings
+              LEFT JOIN customers ON bookings.customerID = customers.customerID
+              WHERE bookingdate >= '2026-07-01'
+              GROUP BY customercountry
+              ORDER BY c DESC";
+
         public ActionResult Index()
         {
-            var db = new Models.Supercontrol2Context();
-            var v = db.Database.SqlQuery<int>("SELECT 1").FirstOrDefault();
+            using (var db = new Supercontrol2Context())
+            {
+                ViewBag.TopLocations = db.Database
+                    .SqlQuery<TopLocationDto>(TopLocationsSql)
+                    .ToList();
+            }
+
             return View();
         }
     }
---
+++ Supercontrol.Web.Dashboard/Supercontrol.Web.Dashboard/Models/TopLocationDto.cs
@@ -0,0 +1,8 @@
+namespace Supercontrol.Web.Dashboard.Models
+{
+    public class TopLocationDto
+    {
+        public string CustomerCountry { get; set; }
+        public int C { get; set; }
+    }
+}
